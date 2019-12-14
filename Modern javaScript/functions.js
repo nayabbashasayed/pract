69e7a91ab50df76fbbfbbbc46084e23b2bf4cb15
Nayab Basha Sayed <nayabbashasyed@gmail.com>
2019-12-14 23:02:30 +0530

    Functions, argument initialization

---
+++ Modern javaScript/functions.js
@@ -0,0 +1,10 @@
+let multipleArg = (a, b, c) => {
+        console.log('a : ' + a);
+        console.log('b : ' + b);
+        console.log('c : ' + c);
+}
+
+multipleArg(1); // a = 1, b and c are undefined
+/* Passing only the second argument */
+multipleArg(undefined, 2); // a and c are undefined. b = 2
+
