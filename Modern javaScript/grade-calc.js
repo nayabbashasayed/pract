e720402291f05e459187bf47a95164d66f566ae3
Nayab Basha Sayed <nayabbashasyed@gmail.com>
2019-12-21 17:28:03 +0530

    A simple grade calculator

---
+++ Modern javaScript/grade-calc.js
@@ -0,0 +1,19 @@
+let calcGrade = (studentScore, totalPossibleScore) => {
+	let percentage = studentScore * 100 / totalPossibleScore;
+	let grade;
+
+	if (percentage >= 90 && percentage <= 100) {
+		grade = 'A';
+	}else if (percentage >= 80 && percentage <= 89) {
+		grade = 'B';
+	}else if (percentage >= 70 && percentage <= 79) {
+		grade = 'C';
+	}else if (percentage >= 60 && percentage <= 69) {
+		grade = 'D';
+	}else if (percentage >= 0 && percentage <= 59) {
+		grade = 'F';
+	}
+	return `You got the grade ${grade} - (${percentage}%)!`;
+}
+
+console.log(calcGrade(12, 20));
