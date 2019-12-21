182116e0a35bc58c38167393948fb7c014c40f5d
Nayab Basha Sayed <nayabbashasyed@gmail.com>
2019-12-21 17:29:58 +0530

    Accessing object members

---
+++ Modern javaScript/objects.js
@@ -0,0 +1,11 @@
+let person = {
+	name : "Nayab",
+	age : 30,
+	location : 'Chennai, India'
+};
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
+
+person.age += 1;
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
