ebadc770be1889e2fce1c51447214c48dbc27fa3
Nayab Basha Sayed <nayabbashasyed@gmail.com>
2019-12-22 21:22:36 +0530

    Two objects never same even though looks same

--- Modern javaScript/objects.js
+++ Modern javaScript/objects.js
@@ -9,3 +9,13 @@ console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
 person.age += 1;
 
 console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
+
+let oneObject = {};
+let twoObject = {};
+
+console.log(oneObject === twoObject); //false
+console.log(oneObject == twoObject); // false
+
+let threeObject = oneObject;
+
+console.log(threeObject == oneObject); //true
